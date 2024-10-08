d643f714f3f1b98876541bd95913dc00d607637d
Kartik Girhepunje <88491408+kartik30G@users.noreply.github.com>
2024-10-08 14:54:19 +0530

    Update worker.service.ts

--- angular-frontend/src/app/services/worker.service.ts
+++ angular-frontend/src/app/services/worker.service.ts
@@ -9,7 +9,7 @@ import { Worker } from '../models/worker';
 })
 export class WorkerService {
 
-  private getUrl: string = "http://localhost:30000/api/v1/workers";
+  private getUrl: string = "http://3.108.53.133:30000/api/v1/workers";
 
   constructor(private _httpClient: HttpClient) { }
 
